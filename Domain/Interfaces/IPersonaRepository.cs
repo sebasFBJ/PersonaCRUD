bc32d28977aa7ce801d4d6dc1b9a635d416ab302
sebas <sebastiabravo3@gmail.com>
2025-08-29 18:37:34 -0500

    clase pojo y Interfaces de persona terminadas

--- Domain/Interfaces/IPersonaRepository.cs
+++ Domain/Interfaces/IPersonaRepository.cs
@@ -2,3 +2,12 @@ using PersonasCRUD.Domain.Entities;
 
 namespace PersonasCRUD.Domain.Interfaces;
 
+public interface IPersonaRepository : IGenericRepository<Persona>
+{
+    // Este metodo busca personas por un rango de edad
+    Task<IEnumerable<Persona>> GetByAgeRangeAsync(int minAge, int maxAge);
+    // Este metodo busca personas por nombre (busqueda parcial)
+    Task<IEnumerable<Persona>> SearchByNameAsync(string name);
+    // Este metodo busca personas por nombre exacto
+    Task<Persona?> GetByNombreAsync(string nombre);
+}
